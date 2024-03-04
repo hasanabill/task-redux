1dd66ea726b1e64bcd095915a8eed581a2964615
hasanabill <hasanabilna@gmail.com>
2024-03-04 15:42:07 +0600

    sliced

--- src/redux/features/tasks/taskSlice.js
+++ src/redux/features/tasks/taskSlice.js
@@ -9,11 +9,23 @@ const taskSlice = createSlice({
     initialState,
     reducers: {
         addTask: (state, { payload }) => {
-            state.tasks.push(payload)
+            if (state.tasks.length === 0) {
+                state.tasks.push({ id: 1, status: 'pending', ...payload });
+            } else {
+                const lastId = state.tasks.at(-1).id;
+                state.tasks.push({
+                    id: lastId + 1,
+                    status: 'pending',
+                    ...payload
+                });
+            }
+        },
+        removeTask: (state, { payload }) => {
+            state.tasks.filter((item) => item.id !== payload);
         }
     },
 })
 
-export const { addTask } = taskSlice.actions;
+export const { addTask, removeTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
